bb3f2842696fe8494006ef017833380e201d359f
Priya Gupta <priya.gupta@mutualmobile.com>
2022-04-25 11:27:13 +0530

    Boilerplate for react-code-test

---
+++ src/components/sample-component.tsx
@@ -0,0 +1,12 @@
+/*
+ * File: sample-component.tsx
+ */
+
+import { ReactElement } from "react";
+import React from "react";
+
+const SampleComponent = (): ReactElement => {
+  return <></>;
+};
+
+export default SampleComponent;
